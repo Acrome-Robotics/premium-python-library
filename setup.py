b5717f4f3b29aa4fadb737df7334c548e9f24127
Berat Computer <berat.dogan@std.yildiz.edu.tr>
2024-07-16 17:14:17 +0300

    renames.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="premium_acrome",
+	name="acrome-premium",
 	version="1.0.0",
 	author="Berat Computer",
 	author_email="beratdogan@acrome.net",
